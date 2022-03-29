fef9d0096fe6e7130930a23eab11491bed54442a
Alex Vong <avong003@gmail.com>
2022-03-28 22:28:26 -0700

    added header and included head php

---
+++ header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang='en'>
+	<?php include('head.php'); ?>
+
+	<body>
+		<header class='site-header'>
+			<inner-column>
+				<masthead>
+					<nav class='site-menu'>
+						<h1 class='loud-voice'>Responsive Layouts in the Wild</h1>
+			
+				</masthead>
+
+			</inner-column>
+		</header>
+
+
+		<main class='page-content'>
+			<!-- STUFF -->
