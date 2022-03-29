da3fff9f66a385b356244c58d6acc853d10e384d
Alex Vong <avong003@gmail.com>
2022-03-28 22:27:42 -0700

    created index.php

---
+++ index.php
@@ -0,0 +1,69 @@
+
+
+<?php include('header.php'); ?>
+
+
+
+
+	<section class='page-section day-1'>
+		<inner-column>
+			<?php include('modules/day-1.php'); ?> 
+		</inner-column>
+	</section>
+
+
+	<section class='page-section starbucks'>
+		<inner-column>
+			<?php include('modules/starbucks.php'); ?> 
+		</inner-column>
+	</section> 
+
+	<section class='page-section onfleet'>
+		<inner-column>
+			<?php include('modules/onfleet.php'); ?> 
+		</inner-column>
+	</section>
+
+	<section class='page-section atmosphere'>
+		<inner-column>
+			<?php include('modules/atmosphere.php'); ?> 
+		</inner-column>
+	</section>
+
+	<section class='page-section slack'>
+		<inner-column>
+			<?php include('modules/slack.php'); ?> 
+		</inner-column>
+	</section>
+
+	<section class='page-section pxl'>
+		<inner-column>
+			<?php include('modules/pxl.php'); ?> 
+		</inner-column>
+	</section> 
+
+
+	<section class='page-section upgrade'>
+		<inner-column>
+			<?php include('modules/upgrade.php'); ?> 
+		</inner-column>
+	</section>
+
+	<section class='page-section disney'>
+		<inner-column>
+			<?php include('modules/disney.php'); ?> 
+		</inner-column>
+	</section>
+
+	<section class='page-section array-site'>
+		<inner-column>
+			<?php include('modules/array-site.php'); ?> 
+		</inner-column>
+	</section>
+
+
+
+
+
+
+
