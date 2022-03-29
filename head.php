2836a976b4b4b927c647318d2f6fc4264dfb3a66
Alex Vong <avong003@gmail.com>
2022-03-28 22:28:36 -0700

    created meta data

---
+++ head.php
@@ -0,0 +1,20 @@
+
+
+<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>30-Day Responsive Layout Challenge</title>
+		<meta
+			name="og:description"
+			content="Improving Responsive Layouts One Day at a Time"
+		/>
+	
+		<meta property="og:title" content="30 Day Responsive Layout Challenge" />
+		<meta property="og:type" content="">
+		<meta property="og:url" content="">
+
+		<meta property="og:image" content="https://peprojects.dev/alpha-1/alexvong/images/30-day.png" />
+		<link rel="shortcut icon" type="image/jpg" href="https://peprojects.dev/alpha-1/alexvong/images/30-day-fav.png" />
+
+		<link rel='stylesheet' href='css/style.css'>
+</head>
