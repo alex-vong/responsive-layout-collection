22d5fbb07be2b4b857293faaaabc0b400635a017
Alex Vong <avong003@gmail.com>
2022-03-29 11:24:52 -0700

    slack challenge

---
+++ modules/slack.php
@@ -0,0 +1,79 @@
+ 
+
+ <h2 class="section-heading calm-voice">Slack</h2>
+
+ <section class="project-info">
+    <h3 class="calm-voice">Source:
+         <a href="https://slack.com/features" target="_blank">Slack's Feature Page</a>
+<!--     </h3>
+        <h3 class="calm-voice"> Read the
+         <a href="https://alexvong.substack.com/p/responsive-layout-challenge-day-1" target="_blank">Case Study</a>
+    </h3> -->
+ </section>
+
+ <section class="container">
+    <div class="content-container">
+        <div class="features-heading">
+            <picture>
+                <img src="images/slack-workspace.png" alt="slack-workspace-puzzle-image">
+            </picture>
+
+            <div class="heading-content">
+                <h2 class="attention-voice">Work more easily with Everyone</h2>
+            <p class="calm-voice">Stay organized and focused with central spaces for conversations, files, tools and people.</p>  
+            </div>
+
+        </div>
+
+
+        <div class="features-content">
+
+            <div class="main-content">
+               <a href="#">
+                   <picture>
+                       <img src="images/slack-hashtag.svg" alt="">
+                   </picture>
+                   <h3 class="calm-voice">Channels</h3>
+                   <p class="quiet-voice">
+                      Collaborate with teams at other companies the same way you do with teams at your own. 
+                   </p>
+               </a>
+               <a href="#">
+                   <picture>
+                       <img src="images/slack-connect.svg" alt="">
+                   </picture>
+                   <h3 class="calm-voice">Slack Connect</h3>
+                   <p class="quiet-voice">
+                      Collaborate with teams at other companies the same way you do with teams at your own. 
+                   </p>
+
+               </a>
+            </div>
+
+            <div class="aside-content">
+               <a href="#">
+                   <picture>
+                       <img src="images/slack-messaging.svg" alt="">
+                   </picture>
+                   <p class="calm-voice">Messaging</p>
+               </a>
+               <a href="#">
+                   <picture>
+                       <img src="images/slack-access.svg" alt="">
+                   </picture>
+                   <p class="calm-voice">Accessibility</p>
+               </a>
+               <a href="#">
+                   <picture>
+                       <img src="images/slack-calls.svg" alt="">
+                   </picture>
+                   <p class="calm-voice">Voice and video calls</p>
+
+               </a>
+            </div>
+        </div>
+
+
+    </div>
+     
+ </section>
