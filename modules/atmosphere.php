07694ab773750d4304b71aca46c392d2d3d435c5
Alex Vong <avong003@gmail.com>
2022-03-29 11:24:33 -0700

    atmospher challenge

---
+++ modules/atmosphere.php
@@ -0,0 +1,62 @@
+    <h2 class="section-heading calm-voice">Atmosphere</h2>
+
+    <section class="project-info">
+    <h3 class="calm-voice">Source:
+        <a href="https://atmosphere.tv/" target="_blank">Atmosphere.tv</a>
+    </h3>
+
+    </section>
+
+
+    <section class="container">
+        <section class="diptych">
+            <picture>
+                <img src="https://images.prismic.io/atmosphere-marketing/84db82bc-9bd8-4707-adfa-c2058fbeb5b0_video-img.png?auto=compress,format" alt="people-staring-at-a-screen">
+            </picture>
+
+            <div class="diptych-text">
+                <h2 class="loud-voice">This is a <span>Diptych</span></h1>
+                <div class="little-border-thing"></div>
+                <p class="calm-voice">Look at this cool diptych. The image take up twice as much as spacing of the text</p>
+            </div>
+
+        </section>
+
+    <section class="info-cards">
+        <info-card>
+            <h3 class="attention-voice">
+                Cool Heading
+            </h3>
+
+            <p class="calm-voice">
+                This is some text talking about some goods or services
+            </p>
+        </info-card>
+
+         <info-card>
+            <h3 class="attention-voice">
+                Cool Heading
+            </h3>
+
+            <p class="calm-voice">
+                This is some text talking about some goods or services
+            </p>
+        </info-card>
+        <info-card>
+            <h3 class="attention-voice">
+                Cool Heading
+            </h3>
+
+            <p class="calm-voice">
+                This is some text talking about some goods or services
+            </p>
+        </info-card>
+
+
+    </section>
+
+    </section>
+
+    <section class="atmosphere-reverse">
+        <?php include('atmosphere-reverse.php') ?>
+    </section>
