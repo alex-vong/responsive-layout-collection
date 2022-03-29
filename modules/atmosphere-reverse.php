6d5c69be3fa293777a5eb44b497a490e661c62e4
Alex Vong <avong003@gmail.com>
2022-03-29 11:24:40 -0700

    atmospher reverse challenge

---
+++ modules/atmosphere-reverse.php
@@ -0,0 +1,57 @@
+ 
+
+    <section class="project-info">
+    <h3 class="calm-voice">Atmosphere but reversed
+    </h3>
+
+    </section>
+
+
+    <section class="container atm-reverse">
+        <section class="diptych reverse">
+            <picture>
+                <img src="https://images.prismic.io/atmosphere-marketing/84db82bc-9bd8-4707-adfa-c2058fbeb5b0_video-img.png?auto=compress,format" alt="people-staring-at-a-screen">
+            </picture>
+
+            <div class="diptych-text">
+                <h2 class="loud-voice">Same but <span>Different</span></h1>
+                <div class="little-border-thing"></div>
+                <p class="calm-voice">This is the same ditpych as above but with some style tweaks</p>
+            </div>
+
+        </section>
+
+    <section class="info-cards">
+        <info-card>
+            <h3 class="attention-voice">
+                Same HTML
+            </h3>
+
+            <p class="calm-voice">
+                This is some text talking about some goods or services
+            </p>
+        </info-card>
+
+         <info-card>
+            <h3 class="attention-voice">
+                Different Styles
+            </h3>
+
+            <p class="calm-voice">
+                This is some text talking about some goods or services
+            </p>
+        </info-card>
+        <info-card>
+            <h3 class="attention-voice">
+                Different Layout
+            </h3>
+
+            <p class="calm-voice">
+                This is some text talking about some goods or services
+            </p>
+        </info-card>
+
+
+    </section>
+
+    </section>
