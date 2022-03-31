56796bf142a0deae4f6862f9ae44a545bf6daa45
Alex Vong <avong003@gmail.com>
2022-03-31 12:38:52 -0700

    data component

---
+++ modules/data.php
@@ -0,0 +1,202 @@
+
+<?php
+
+	$rewards = [
+
+		[
+			"card-image" => "images/upgradecard-rewards.png",
+			"card-heading" => "Cash Rewards",
+			"card-subheading" => "Unlimited 1.5% cash back on payments",
+			"card-caption" => "Average to Excellent Credit",
+			"more-info" => "Get more info",
+
+			"fees" => "No Fees",
+		],
+
+		[
+			"card-image" =>"images/upgradecard-triple-cash.png",
+			"card-heading" => "Cash Rewards",
+			"card-subheading" => "Unlimited 3% cash back on payments for purchases in Home, Auto, and Health categories",
+			"card-caption" => "Average to Excellent Credit",
+			"more-info" => "Get more info",
+			"fees" => "No Fees",
+
+
+		],
+
+
+		[
+			"card-image" =>"images/upgrade-card-bitcoin.png",
+			"card-heading" => "Bitcoin Rewards",
+			"card-subheading" => "Unlimited 1.5% cash back on payments",
+			"card-caption" => "Average to Excellent Credit",
+			"more-info" => "Get more info",
+			"fees" => "No fees except bitcoin sales fee",
+			
+		],
+
+		[
+			"card-image" => "images/upgradecard-rewards.png",
+			"card-heading" => "Upgrade Card",
+			"card-subheading" => "Get the credit you need with no fees",
+			"card-caption" => "Improving credit",
+			"more-info" => "Get more info",
+			"fees" => "No Fees",
+			
+		]
+
+	];
+
+	$pxlData = [
+		[
+			"id" => "pxl-platform",
+			"pxl-image" => "images/pxl-platform2.svg",
+			"pxl-header" =>"PXL Platform",
+			"pxl-body-text" => "The PXL Platform is a robust website system featuring a fully-custom content management interface. It’s designed to be used as the foundation for any enterprise B2C or B2B website, and is the backbone of sites that have been visited over 1 billion times since 2015.",
+			"pxl-body-text2" => false,
+
+		],
+
+		[
+			"id" => "bracket-contest",
+			"pxl-image" => "images/pxl-platform.jpeg",
+			"pxl-header" =>"Bracket Contest Platform",
+			"pxl-body-text" => "Quickly launch a “March Madness” inspired digital campaign with our online bracket generator and moderation tools. Fans can set their brackets, select their winner, and share their pick with the world.",
+			"pxl-body-text2" => false,
+
+
+		],
+
+		[
+			"id" => "selfie",
+			"pxl-image" => "images/selfie.jpeg",
+			"pxl-header" =>"Red Carpet Selfie Booth",
+			"pxl-body-text" => "It’s more than a camera on the red carpet - the Selfie Booth produces fun, expressive branded social content with on-voice creative that is guaranteed to be shared by fans",
+			"pxl-body-text2" => false,
+
+
+		],
+
+		[
+			"id" => "u-review",
+			"pxl-image" => "images/pxl-mobile-gif.gif",
+			"pxl-header" =>"U Review",
+			"pxl-body-text" => "The PXL Platform is a robust website system featuring a fully-custom content management interface. It’s designed to be used as the foundation for any enterprise B2C or B2B website, and is the backbone of sites that have been visited over 1 billion times since 2015.",
+			"pxl-body-text2" => "Amplify social chatter with U Review, a social media tool for quickly responding to positive comments with personalized high-end motion graphics.",
+
+		],
+		// [
+		// 	"id" => "meme-generator",
+		// 	"pxl-image" => "images/meme.jpeg",
+		// 	"pxl-header" =>"Meme Generator",
+		// 	"pxl-body-text" => "Make some memes! PXL’s Meme Generator web application gets fans sharing your content with a personal twist. Outputting either still or video content, fans can share their asset from the Meme Generator direct to Facebook and Twitter, or download with 1-click.",
+		// 	"pxl-body-text2" => "The app can be configured to place user photos underneath themed content as well as add digital stickers and text on top.",
+
+		// ],
+
+		// [
+		// 	"id" => "quiz-game",
+		// 	"pxl-image" => "images/pxl-quiz-game.png",
+		// 	"pxl-header" =>"Quiz Game",
+		// 	"pxl-body-text" => "Challenge fans with trivia or create a personalized experience with self-reflection questions using PXL’s multiple choice quiz game. The game can be played on Facebook or in the browser, ending with a sharable score asset to encourage word of mouth marketing.",
+		// 	"pxl-body-text2" => false,
+
+
+		// ],
+	];
+
+
+
+	$disney = [
+
+		[
+			"movie-poster" => "https://lumiere-a.akamaihd.net/v1/images/cg_disney_shangchi_horizontal_22257_a30c23c4.jpeg?region=0,0,1460,824&width=768",
+			"movie-title" => "Shang-Chi and the Legend of the Ten Rings",
+			"movie-description" => "Experience the beginning of a new Marvel super Hero. Now streaming on Disney+."
+		],
+
+			[
+			"movie-poster" => "https://lumiere-a.akamaihd.net/v1/images/cg_junglecruise_disneyplus_horizontal_21349_d4bff495.jpeg?region=0,0,1460,824&width=768",
+			"movie-title" => "Jungle Cruise",
+			"movie-description" => "The adventure of a lifetime is now streaming on Disney+."
+		],
+	];
+
+
+	$array = [
+
+		[
+			"id" => "top-row",
+			"image" => "https://www.arraylive.com/hs-fs/hubfs/Ellipse%201.png?width=294&name=Ellipse%201.png%20294w",
+			"card-heading" => "1. Schedule a Consultation",
+			"card-text" => "Tell us your needs and event goals, especially regarding your audience and presenter experience. We’ll then demo the software and help you choose the right options for you event."
+		],
+		[
+			"id" => "middle-row",
+			"image" => "https://www.arraylive.com/hs-fs/hubfs/Ellipse%201.png?width=294&name=Ellipse%201.png%20294w",
+			"card-heading" => "2. Get Your Content Engagement Solution",
+			"card-text" => "Before your event, we advise on best practices, set-up the technology, and provide training and support for your team—everything you need to deliver great content and keep your audience engaged, whether in-person or virtual"
+		],
+		[
+			"id" => "bottom-row",
+			"image" => "https://www.arraylive.com/hs-fs/hubfs/Layer%2025.png?width=294&name=Layer%2025.png 294w",
+			"card-heading" => "3. Deliver Successful Events",
+			"card-text" => "We manage everything - technology, presenter and attendee support, and moderation. After the event, we also provide analytics on every audience member’s engagement and even offer ways to improve the next one."
+		],
+
+	];
+
+
+
+
+	$onfleet = [
+
+		[
+			
+			"image" => "https://d1er272rpp2pqg.cloudfront.net/s4a98f282/images/pricing/bike.png",
+			"level" => "Starter",
+			"price" => "$149/mo",
+			"card-text" => "Get to know Onlfeet. Track, dispatch, manage, and analyze your fleet.",
+			"button-text" => "Start free trial",
+		],
+		[
+			
+			"image" => "https://d1er272rpp2pqg.cloudfront.net/s4a98f282/images/pricing/scooter.png",
+			"level" => "Basic",
+			"price" => "$349/mo",
+			"card-text" => "Starter + optimize routes, chat with drivers and build integrations.",
+			"button-text" => "Start free trial",
+		],
+		[
+			
+			"image" => "https://d1er272rpp2pqg.cloudfront.net/s4a98f282/images/pricing/car.png",
+			"level" => "Premium",
+			"price" => "$799/mo",
+			"card-text" => "Basic + scan barcodes and IDs, and use a dedicated phone number.",
+			"button-text" => "Contact Sales",
+		],
+		[
+			
+			"image" => "https://d1er272rpp2pqg.cloudfront.net/s4a98f282/images/pricing/truck.png",
+			"level" => "Professional",
+			"price" => "$1,999/mo",
+			"card-text" => "Premium + priority support, full data access and brand customization.",
+			"button-text" => "Contact Sales",
+		],
+
+	];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
