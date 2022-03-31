e631b4ca90e8d1314bea864b2ebe9ace676c7c33
Alex Vong <avong003@gmail.com>
2022-03-31 12:38:13 -0700

    arrive responsive component

---
+++ modules/arrive.php
@@ -0,0 +1,34 @@
+<h2 class="section-heading calm-voice">Arrive Responsive Cards</h2>
+
+    <section class="project-info">
+    <h3 class="calm-voice">Source:
+        <a href="https://www.arrivelogistics.com/" target="_blank">Arrivelogistics</a>
+    </h3>
+
+    </section>
+
+
+    <section class="container">
+
+    	<call-to-action>
+    		<card>
+    			<h3 class="quiet-voice">Cool Heading</h3>
+    			<p class="calm-voice">"Super cool quote talking about the product</p>
+    			<p class="quiet-voice"><span>- Person Who Said Quote</span></p>
+
+    			<button>Generic Button</button>
+    		</card>
+    	</call-to-action>
+
+    	<call-to-action>
+    		<card>
+    			<h3 class="quiet-voice">Cool Heading</h3>
+    			<p class="calm-voice">"Super cool quote talking about the product</p>
+    			<p class="quiet-voice"><span>- Person Who Said Quote</span></p>
+
+    			<button>Generic Button</button>
+    		</card>
+    	</call-to-action>
+        
+
+    </section>
