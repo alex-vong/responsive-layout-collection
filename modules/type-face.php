dc56d5e2ea49916b9e49f0fa01b416d0c806dc58
Alex Vong <avong003@gmail.com>
2022-03-31 12:38:22 -0700

    adding typefaces

---
+++ modules/type-face.php
@@ -0,0 +1,79 @@
+
+  <h2 class="section-heading attention-voice">Type faces</h2>
+
+  <div class="font heading">
+    <h2 class="flag">Montserrat</h2>
+
+    <div class="large">
+      <div class="letters upper-case">
+        abcdefghijklmnopqrstuvwxyz
+      </div>
+      <div class="letters lower-case">
+        abcdefghijklmnopqrstuvwxyz
+      </div>
+      <div class="symbols">
+        !@#$%^&amp;*()_+[]{}: “ ” &lt;&gt;'?,.\/:;</div>
+      <div class="numbers">
+        12345678910</div>
+    </div>
+
+    <div class="normal">
+      <div class="letters upper-case">
+        abcdefghijklmnopqrstuvwxyz
+      </div>
+    <div class="letters">
+        abcdefghijklmnopqrstuvwxyz
+      </div>
+
+      <div class="symbols">
+        !@#$%^&amp;*()_+[]{}: “ ” &lt;&gt;'?,.\/:;</div>
+      <div class="numbers">
+        12345678910</div>
+    </div>
+  </div>
+
+  <div class="font body">
+    <h2 class="flag">Roboto Slab</h2>
+
+    <div class="large calm-voice">
+      <div class="letters upper-case">
+        abcdefghijklmnopqrstuvwxyz</div>
+      <div class="letters uppercase">
+        abcdefghijklmnopqrstuvwxyz</div>
+      <div class="symbols">
+        !@#$%^&amp;*()_+[]{}: “ ” &lt;&gt;'?,.\/:;</div>
+      <div class="numbers">
+        12345678910</div>
+    </div>
+
+    <div class="normal calm-voice">
+      <div class="letters upper-case">
+        abcdefghijklmnopqrstuvwxyz</div>
+      <div class="letters uppercase">
+        abcdefghijklmnopqrstuvwxyz</div>
+      <div class="symbols">
+        !@#$%^&amp;*()_+[]{}: “ ” &lt;&gt;'?,.\/:;</div>
+      <div class="numbers">
+        12345678910</div>
+    </div>
+  </div>
+
+  <div class="example-text body">
+    <h2 class="flag">Example text</h2>
+    <h2 class="loud-voice">Hi, I'm <span>Alex&nbsp;Vong</span></h2>
+    <h3 class="attention-voice">I create amazing experiences 
+through <span>Design</span> and <span>Development</span></h3>
+
+    <p class="calm-voice">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat omnis magnam <span>buzz word</span> suscipit perferendis? Illo qui voluptatibus hic asperiores dignissimos, consequuntur commodi, omnis harum aspernatur minus sequi blanditiis quisquam <span>buzz word</span>  recusandae vel corporis, veniam dolor. Nobis consequuntur quibusdam sit eligendi dolorum adipisci necessitatibus blanditiis est, minus, <span>buzz word</span> atque ut corporis modi quas ea.
+    </p>
+    <p class="calm-voice">
+      Lorem, <span>buzz word</span> ipsum dolor sit amet consectetur adipisicing, elit. Excepturi at quas quibusdam voluptate ex fuga, unde perferendis <span>buzz word</span> quod natus quam rem, distinctio, odit commodi dicta.
+    </p>
+    <p class="calm-voice">
+      The quick brown fox jumped over the lazy fox.
+    </p>
+      <a href="#" class='btn btn-secondary'>Button</a>
+  </div>
+
+
