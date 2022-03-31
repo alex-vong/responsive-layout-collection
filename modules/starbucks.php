a313467076ae225d70484d025bad998aa4afe06b
Alex Vong <avong003@gmail.com>
2022-03-31 12:38:28 -0700

    starbucks component

---
+++ modules/starbucks.php
@@ -0,0 +1,56 @@
+ <h2 class="section-heading calm-voice">Starbucks</h2>
+
+ <section class="project-info">
+    <h3 class="calm-voice">Source:
+         <a href="https://www.starbucks.com/" target="_blank">Starbucks</a>
+<!--     </h3>
+        <h3 class="calm-voice"> Read the
+         <a href="https://alexvong.substack.com/p/responsive-layout-challenge-day-1" target="_blank">Case Study</a>
+    </h3> -->
+ </section>
+
+
+ <section class="container">
+     <section class="featured-product">
+         <picture>
+             <img src="images/sb-main-bg.png" alt="starbucks-coffee-bg">
+         </picture>
+
+         <div class="card-details">
+             <h2 class="attention-voice">Sweet Responsive Layout</h2>
+             <h3 class="calm-voice">See the way Alex was able to recreate this starbucks module. He created these three components that will stack on top eachother as they screen size gets smaller.</h3>
+
+             <button><a href="#">Hire Alex Today</a></button>
+         </div>
+     </section>
+
+
+     <section class="side-content">
+         <section class="rewards-card">
+             <picture>
+                 <img src="images/sb-payment-image.jpeg" alt="sb-rewards-image">
+             </picture>
+
+             <div class="card-details">
+                 <h2 class="attention-voice">Alex is a star and can make your company $</h2>
+                 <p class="calm-voice">With a <span>passion for responsive layouts</span>, Alex can make sure your business' site can be see on all devices.</p>
+
+                 <button><a href="#">Learn more</a></button>
+             </div>
+         </section>
+
+         <section class="mobile-app rewards-card">
+             <picture>
+                 <img src="images/sb-mobile-app.jpeg" alt="sb-rewards-image">
+             </picture>
+
+             <div class="card-details">
+                 <h2 class="attention-voice">Alex turns coffee into design and code</h2>
+                 <p class="calm-voice">It's not magic, it's hundreds of hours of practice.</p>
+
+                 <button><a href="#">See pickup options</a></button>
+             </div>
+         </section>
+     </section>
+
+ </section>
