fe6e3ed04c094b4f4e2d4214f0192b43eb94d933
Alex Vong <avong003@gmail.com>
2022-04-02 17:36:24 -0700

    colorhuddle challenge

---
+++ modules/colorhuddle.php
@@ -0,0 +1,44 @@
+<h2 class="section-heading calm-voice">ColorHuddle</h2>
+
+    <section class="project-info">
+    <h3 class="calm-voice">Source:
+        <a href="https://colorhuddle.co/" target="_blank">ColorHuddle</a>
+    </h3>
+
+    </section>
+
+
+    <section class="container">
+
+    	<cta>
+    		<card>
+                <p class="quiet-voice"><span>Text Above Heading</span></p>
+    			<h3 class="attention-voice">Super Cool Heading</h3>
+    			<p class="calm-voice">A paragraph about this CTA and why the reader should care</p>
+    			<button>Generic Button</button>
+    		</card>
+
+            <cta-image>
+                <picture>
+                    <img src="../images/no-face.svg" alt="">
+                </picture>
+            </cta-image>
+    	</cta>
+
+        <cta>
+            <card>
+                <p class="quiet-voice"><span>Text Above Heading</span></p>
+                <h3 class="attention-voice">Super Cool Heading</h3>
+                <p class="calm-voice">Look I imported my own SVG to show I know how to import them!</p>
+                <button>Generic Button</button>
+            </card>
+
+            <cta-image>
+                <picture>
+                    <img src="../images/mushroom-skull.svg" alt="">
+                </picture>
+            </cta-image>
+        </cta>
+        
+
+    </section>
