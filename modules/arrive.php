caf38cc5c0d9d0ace3978137cf437e69de645f93
Alex Vong <avong003@gmail.com>
2022-04-02 17:36:37 -0700

    updated quotes on arive

--- modules/arrive.php
+++ modules/arrive.php
@@ -23,7 +23,7 @@
     	<call-to-action>
     		<card>
     			<h3 class="quiet-voice">Cool Heading</h3>
-    			<p class="calm-voice">"Super cool quote talking about the product</p>
+    			<p class="calm-voice">"Super cool quote talking about the product"</p>
     			<p class="quiet-voice"><span>- Person Who Said Quote</span></p>
 
     			<button>Generic Button</button>
